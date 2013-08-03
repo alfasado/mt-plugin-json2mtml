dc58e8a5c9043633997199114671d60a5df56cfb
Alfasado Inc <webmaster@alfasado.jp>
2013-08-03 17:52:26 +0900

    first commit

---
+++ plugins/JSON2MTML/lib/JSON2MTML/L10N/ja.pm
@@ -0,0 +1,10 @@
+package JSON2MTML::L10N::ja;
+use strict;
+use base 'JSON2MTML::L10N';
+use vars qw( %Lexicon );
+
+our %Lexicon = (
+    'JSON to MTML using Movable Type data api.' => 'Movable Type data apiをMTML出力します。',
+ );
+
+1;
---
+++ plugins/JSON2MTML/lib/JSON2MTML/Tags.pm
@@ -0,0 +1,95 @@
+package JSON2MTML::Tags;
+use strict;
+use HTTP::Request::Common;
+use LWP::UserAgent;
+use JSON qw/decode_json/;
+no warnings 'redefine';
+use Data::Dumper;
+{
+    package Data::Dumper;
+    sub qquote { return shift; }
+}
+$Data::Dumper::Useperl = 1;
+
+sub _hdlr_json2mtml {
+    my ( $ctx, $args, $cond ) = @_;
+    my $app = MT->instance;
+    my $api_version = $app->config( 'DataAPIVersion' );
+    my $instance_url = $args->{ instance };
+    my $request = $args->{ request };
+    if (! $instance_url ) {
+        $instance_url = $app->config( 'DataAPIURL' );
+    }
+    my $api = "${instance_url}/${api_version}${request}";
+    my $req = HTTP::Request->new( 'GET', $api );
+    my $ua = LWP::UserAgent->new;
+    my $res = $ua->request( $req );
+    my $json;
+    my ( $code, $message );
+    if ( $res->is_error ) {
+        return '';
+    } else {
+        $res = $res->{ _content };
+        $json = decode_json( $res );
+        if ( $args->{ debug } ) {
+            my $res = '<pre>' . $api . ':';
+            $res .= Dumper( $json );
+            $res .= '</pre>';
+            return $res;
+        }
+        if ( my $error = $json->{ error } ) {
+            $code = $error->{ code };
+            $message = $error->{ message };
+        } else {
+            $json = $json->{ items };
+        }
+    }
+    my $res = '';
+    my $vars = $ctx->{ __stash }{ vars } ||= +{};
+    my $tokens = $ctx->stash( 'tokens' );
+    my $builder = $ctx->stash( 'builder' );
+    if (! $code && $json ) {
+        $ctx->stash( 'json2mtmlitems', $json );
+        my $i = 0;
+        for my $record ( @$json ) {
+            my $last = !defined @$json[ $i + 1 ];
+            local $vars->{ __first__ }   = !$i;
+            local $vars->{ __counter__ } = $i + 1;
+            local $vars->{ __odd__ }     = $i % 2 ? 0 : 1;
+            local $vars->{ __even__ }    = $i % 2;
+            local $vars->{ __last__ }    = $last;
+            for my $key ( keys ( %$record ) ) {
+                my $value = $record->{ $key };
+                $vars->{ lc( $key ) } = $value;
+            }
+            my $out = $builder->build( $ctx, $tokens, $cond );
+            if ( !defined( $out ) ) { return $ctx->error( $builder->errstr ) };
+            $res .= $out;
+            $i++;
+        }
+        return $res;
+    } else {
+        $vars->{ code } = $code;
+        $vars->{ message } = $message;
+        my $out = $builder->build( $ctx, $tokens, $cond );
+        if ( !defined( $out ) ) { return $ctx->error( $builder->errstr ) };
+        return $out;
+    }
+}
+
+sub _hdlr_vars_recurse {
+    my ( $ctx, $args, $cond ) = @_;
+    my $key = $args->{ key };
+    my $record = $ctx->{ __stash }{ vars }->{ $key };
+    my $tokens = $ctx->stash( 'tokens' );
+    my $builder = $ctx->stash( 'builder' );
+    for my $key ( keys ( %$record ) ) {
+        my $value = $record->{ $key };
+        $ctx->{ __stash }{ vars }->{ lc( $key ) } = $value;
+    }
+    my $out = $builder->build( $ctx, $tokens, $cond );
+    if ( !defined( $out ) ) { return $ctx->error( $builder->errstr ) };
+    return $out;
+}
+
+1;
